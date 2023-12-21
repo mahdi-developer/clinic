ce7e0ac41a8a4650947318b6a786dbcb4af8a729
Mahdi <muhammadmahdiulislam13@gmail.com>
2023-12-21 09:58:42 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Header scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+};
+
+// Navbar hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    });
+});
